e1ee326b41c415fddd75876993e3540184c1ce8d
Elias Hansen <pe_lias@msn.com>
2016-05-21 11:58:33 +0200

    added nodemodules

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// Inspiration from (for future reference):
+// http://stackoverflow.com/questions/4295782/how-do-you-extract-post-data-in-node-js
+
 var api_key = 'key-d956cf5597f0751aaac1d4593ad6b990';
 var domain = 'sandboxae45f31018c7420aa1a8f72e463e52c8.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 var data = {
-    from: 'Excited User <me@samples.mailgun.org>',
-    to: 'pe_lias@msn.com',
-    subject: 'Hello',
-    text: 'Testing some Mailgun awesomness!'
+    from: 'Tival <no-reply@tival.se>',
+    to: 'salmin89@hotmail.com',
+    subject: 'Tival orderbekräftelse',
+    text: '<b>Här kommer din orderbekräftelse.</b>'
 };
 
 mailgun.messages().send(data, function (error, body) {
